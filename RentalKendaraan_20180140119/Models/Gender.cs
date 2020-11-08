cf9fa6559d7b4998d0d292ae69774b3925a30cd1
Arif Budiman Arrosyid <48200892+arifbudimanarrosyid@users.noreply.github.com>
2020-11-08 20:43:15 +0700

    Validasi

--- RentalKendaraan_20180140119/Models/Gender.cs
+++ RentalKendaraan_20180140119/Models/Gender.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan_20180140119.Models
 {
     public partial class Gender
     {
         public int IdGender { get; set; }
+
+        [Required(ErrorMessage = "Nama Gender tidak boleh kosong")]
         public string NamaGender { get; set; }
 
         public Customer IdGenderNavigation { get; set; }
--- RentalKendaraan_20180140119/Models/Jaminan.cs
+++ RentalKendaraan_20180140119/Models/Jaminan.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan_20180140119.Models
 {
     public partial class Jaminan
     {
         public int IdJaminan { get; set; }
+
+        [Required(ErrorMessage = "Nama jaminan tidak boleh kosong")]
         public string NamaJaminan { get; set; }
 
         public Peminjaman Peminjaman { get; set; }
