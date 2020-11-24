3986ac10fd96efbead6b7d69e06bb17a4360a429
Arif Budiman Arrosyid <48200892+arifbudimanarrosyid@users.noreply.github.com>
2020-11-24 21:00:18 +0700

    Pertemuan 7

--- RentalKendaraan_20180140119/Controllers/KendaraansController.cs
+++ RentalKendaraan_20180140119/Controllers/KendaraansController.cs
@@ -19,10 +19,29 @@ namespace RentalKendaraan_20180140119.Controllers
         }
 
         // GET: Kendaraans
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string ktsd, string searchString)
         {
-            var rental_KendaraanContext = _context.Kendaraan.Include(k => k.IdKendaraanNavigation);
-            return View(await rental_KendaraanContext.ToListAsync());
+            var ktsdList = new List<string>();
+            var ktsdQuery = from d in _context.Kendaraan orderby d.Ketersediaan select d.Ketersediaan;
+            ktsdList.AddRange(ktsdQuery.Distinct());
+            ViewBag.ktsd = new SelectList(ktsdList);
+            var menu = from m in _context.Kendaraan.Include(k => k.IdKendaraanNavigation) select m;
+            if (!string.IsNullOrEmpty(ktsd))
+            {
+                menu = menu.Where(x => x.Ketersediaan == ktsd);
+            }
+
+            return View(await menu.ToListAsync());
+
+            //untuk search data
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                menu = menu.Where(s => s.NoPolisi.Contains(searchString) || s.NamaKendaraan.Contains(searchString)
+                || s.NoStnk.Contains(searchString));
+            }
+
+            //var rental_KendaraanContext = _context.Kendaraan.Include(k => k.IdKendaraanNavigation);
+            //return View(await rental_KendaraanContext.ToListAsync());
         }
 
         // GET: Kendaraans/Details/5
